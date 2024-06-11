e015027e0c38184ab937159b22de99063dca2371
FrTrainingSakshamBhupal <fr.saksham.bhupal@prolim.in>
2024-06-11 17:05:07 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -89,12 +90,79 @@ public class Main {
         System.out.println("Closing your application... \nThank you!");
             }
     private static void searchExpenses(ArrayList<Integer> arrayList) {
-        int leng = arrayList.size();
+        int length = arrayList.size();
         System.out.println("Enter the expense you need to search:\t");
+        
         //Complete the method
+        // Search for the expense entered by the user in the arrayList and print the result.
+
+        Scanner sc = new Scanner(System.in);
+        int search = sc.nextInt();
+        if(arrayList.contains(search)){
+            System.out.println("Expense found: "+search);
+        }else{
+            System.out.println("Expense not found: "+search);
+        }
+
+
+
     }
     private static void sortExpenses(ArrayList<Integer> arrayList) {
         int arrlength =  arrayList.size();
        //Complete the method. The expenses should be sorted in ascending order.
+
+       // Sort expenses in ascending order using mergesort
+        mergeSort(arrayList, 0, arrlength-1);
+        System.out.println("Expenses in ascending order: "+arrayList);
+
+    }
+
+    private static void mergeSort(ArrayList<Integer> arrayList, int l, int r){
+        if(l<r){
+            int m = (l+r)/2;
+            mergeSort(arrayList, l, m);
+            mergeSort(arrayList, m+1, r);
+            merge(arrayList, l, m, r);
+        }
+    }
+
+    private static void merge(ArrayList<Integer> arrayList, int l, int m, int r){
+        int n1 = m-l+1;
+        int n2 = r-m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for(int i=0; i<n1; i++){
+            L[i] = arrayList.get(l+i);
+        }
+        for(int j=0; j<n2; j++){
+            R[j] = arrayList.get(m+1+j);
+        }
+
+        int i=0, j=0;
+        int k=l;
+        while(i<n1 && j<n2){
+            if(L[i]<=R[j]){
+                arrayList.set(k, L[i]);
+                i++;
+            }else{
+                arrayList.set(k, R[j]);
+                j++;
+            }
+            k++;
+        }
+
+        while(i<n1){
+            arrayList.set(k, L[i]);
+            i++;
+            k++;
+        }
+
+        while(j<n2){
+            arrayList.set(k, R[j]);
+            j++;
+            k++;
+        }
     }
 }
